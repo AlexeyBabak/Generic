0450723a7ca994c1b42c23406aa70097399a2100
Alexey Babak <alexey.babak@myib.com>
2020-09-21 21:03:16 +0300

    Add project files.

---
+++ Generic/DeliveryHouse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class DeliveryHouse
+    {
+
+        public T GetBirth<T>(T warmBlooded) where T : WarmBlooded, new()
+        {
+            var newBorn = new T();
+            newBorn.Age = 0;
+            newBorn.Height = warmBlooded.Height / 10;
+
+            return newBorn;
+
+        }
+    }
+
+    public class Human : WarmBlooded
+    {
+        public bool IsWoman { get; set; }
+        public override bool IsPregnant => true;
+    }
+
+    public class Animal : WarmBlooded
+    {
+        public override bool IsPregnant => true;
+    }
+
+    public abstract class WarmBlooded
+    {
+        public int Age { get; set; }
+        public int Height { get; set; }
+        public abstract bool IsPregnant { get; }
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var deliveryHouse = new DeliveryHouse();
+
+            var animalMother = new Animal { Age = 33, Height = 180 };
+            var humanMother = new Human { Age = 33, Height = 180 };
+
+            deliveryHouse.GetBirth(animalMother);
+            deliveryHouse.GetBirth(humanMother);
+
+            var babyAnimal = deliveryHouse.GetBirth(animalMother);
+            var humanAnimal = deliveryHouse.GetBirth(humanMother);
+        }
+    }
+}
